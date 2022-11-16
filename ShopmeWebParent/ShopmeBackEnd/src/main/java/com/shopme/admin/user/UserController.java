b22d2c473a4c4b7d91b318398b66797d69ac07ba
gmkred <mallireddy1999916@gmail.com>
2022-11-17 00:11:26 +0530

    user_form is in progress

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
 @Controller
@@ -14,11 +16,26 @@ public class UserController {
 	@Autowired
 	private UserService service;
 
-	@GetMapping("/user")
+	@GetMapping("/users")
 	public String listAll(Model model) {
 		List<User> listUsers = service.listAll();
 		model.addAttribute("listUsers", listUsers);
 		return "users";
 	}
 
+	@GetMapping("/users/new")
+	public String newUser(Model model) {
+		List<Role> listRoles = service.listRoles();
+		User user = new User();
+		user.setEnabled(true);
+		model.addAttribute("listRoles", listRoles);
+		model.addAttribute("user", user);
+		return "user_form";
+	}
+
+	@PostMapping("/users/save")
+	public String saveUser(User user) {
+		System.out.println(user);
+		return "redirect:/users";
+	}
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserService.java
@@ -3,15 +3,21 @@ package com.shopme.admin.user;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.shopme.common.entity.User;
+import com.shopme.common.entity.Role;
 
 @Service
 public class UserService {
 	@Autowired
-	private UserRepository repo;
+	private UserRepository userRepo;
+	@Autowired
+	private RoleRepository roleRepo;
 
 	public List<User> listAll() {
-		return (List<User>) repo.findAll();
+		return (List<User>) userRepo.findAll();
+	}
+
+	public List<Role> listRoles() {
+		return (List<Role>) roleRepo.findAll();
 	}
 }
